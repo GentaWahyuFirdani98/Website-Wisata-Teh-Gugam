463607bc3f38876427a3374cba802d60c490a5bf
fikrihaikalm <fikrihaikal2105@gmail.com>
2025-05-27 21:13:15 +0700

    add icon

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', 'Beranda') | Admin - {{ config('app.name') }}</title> 
+    <link rel="icon" href="{{ asset('images/icon_war.ico') }}" type="image/x-icon">
     
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,8 @@
     <title>@yield('title') - Kebun Teh Gunung Gambir</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="icon" href="{{ asset('images/icon_war.ico') }}" type="image/x-icon">
+
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     @stack('styles')
 </head>
